21847b91fec3e722257d17409050d9faac2e1dda
kuldeepj14 <jainkuldeep1407@gmail.com>
2022-09-05 23:59:40 +0530

    Completed Python first assignment

--- PythonAssignments/Assignment1.py
+++ PythonAssignments/Assignment1.py
@@ -34,4 +34,44 @@ def findMaxNum():
         print(f"{y} is greater then {x}.")
     else:
         print(f"both numbers are equal.")
-findMaxNum()
+# findMaxNum()
+
+
+# 4. Python Program for factorial of a number
+def numFactorial():
+    num = int(input("enter the number: "))
+    fac = 1
+    if num<0:
+        print("Factorial does not exist for negative numbers")
+    elif num==0:
+        print("Factorial of 0 is 1")
+    else:
+        for i in range(1, num+1):
+            fac = fac * i
+        print(f"Factorial of {num} is {fac}")
+# numFactorial()
+
+
+# 5. Python Program for simple interest
+def findSimpleInterest():
+    principal = int(input("What is the amount: "))
+    interest = float(input("What is the interest: "))
+    time = float(input("What is the duration for which the principal amount is given: "))
+    simpleInterest = ( principal * interest * time ) / 100
+    print(f"Simple interest s: {simpleInterest}")
+# findSimpleInterest()
+
+
+# 6. Python Program for compound interest
+def findCompundInterest():
+    principal = int(input("What is the amount: "))
+    interest = float(input("What is the interest: "))
+    time = float(input("What is the duration for which the principal amount is given: "))
+    amount = principal * (1 + (interest/100)) ** time
+    compoundInterest = amount - principal
+    print(f"Compound Interest Value: {compoundInterest}")
+# findCompundInterest()
+
+
+# 7. Python Program to check Armstrong Number
+
